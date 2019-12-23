74d17fe0eeb63e0d0aec10e6c88907bfe6472449
YR <1106149466@qq.com>
2019-12-23 15:05:26 +0800

    使用element-ui和路由依赖

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+// 1.router文件夹 （当你导入一个文件夹）默认会加载文件夹下得index.js文件
+// 1.1 index.js 含义 索引文件（默认会访问得文件）
+// 1.2 index.js index.vue index.json 支持三种格式，优先级与这里得顺序一致
+// 2.0 vue-cli 提高一个绝对路径别名  src绝对路径
+import router from '@/router'
 
+Vue.use(ElementUI)
+
+// 日志的详情与否，生产环境 开发环境
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
+
+// main.js 入口文件
+// 职责：做全局范围内的依赖和配置
+// 职责：创建一个根VUE实例
