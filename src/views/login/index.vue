56a5ccb43f71183548c43bd4f74fa077808e12a4
YR <1106149466@qq.com>
2019-12-23 18:07:42 +0800

    登录j页面界面完成

---
+++ src/views/login/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container-login">
+    <el-card class="my-card">
+      <img src="../../assets/logo_index.png" alt="">
+      <!-- 表单 -->
+      <el-form :model="loginForm">
+        <el-form-item>
+          <el-input v-model="loginForm.moblile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:230px;margin-right:10px"></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已经阅读和同意用户协议条款和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.container-login{
+  width: 100%;
+  height: 100%;
+  // 让百分百尺寸基于window计算
+  position: absolute;
+  left: 0;
+  top: 0;
+  // background-color: pink;
+  background: url(../../assets/login_bg.jpg) no-repeat center / cover;
+  .my-card{
+    width: 400px;
+    height: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-60%);
+    img{
+      display: block;
+      width: 200px;
+      margin: 0 auto 20px;
+    }
+  }
+}
+
+</style>
---
+++ src/views/login/index1.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class='container-login'>
+    <el-card class="my-card">
+      <img src="../../assets/logo_index.png" alt="">
+      <!-- 表单 -->
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang='less'>
+.container-login{
+  width: 100%;
+  height: 100%;
+  // 让百分百尺寸基于window计算
+  position: absolute;
+  left: 0;
+  top: 0;
+  background:url(../../assets/login_bg.jpg) no-repeat center / cover;
+  .my-card{
+    width: 400px;
+    height: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-60%);
+    img{
+      display: block;
+      width: 200px;
+      margin: 0 auto;
+    }
+  }
+}
+</style>
